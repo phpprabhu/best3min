42dd609293bdc8d2e4a9d6a2019380eb7047963b
Prabhu <prabhu.k@hanatechpvt.com>
2024-12-31 09:39:44 +0530

    removed date delta 2 days

--- exchange/angel.py
+++ exchange/angel.py
@@ -360,7 +360,8 @@ def get_3min_olhcv(angel_obj, index):
     df_index = get_historical_data(angel_obj, index.token, timeframe, nse_interval, 750)
     df_index['timestamp'] = pd.to_datetime(df_index['timestamp'])
     # Remove the delta for current date
-    today = (pd.Timestamp.now() - pd.Timedelta(days=2)).date()
+    # today = (pd.Timestamp.now() - pd.Timedelta(days=2)).date()
+    today = pd.Timestamp.now().date()
     filtered_df = df_index[df_index['timestamp'].dt.date == today]
     # Remove the first 2 rows
     filtered_df = filtered_df.iloc[2:].reset_index(drop=True)
